7f022f3165019107c700bd58d4ae1b52b1f391c3
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-01 23:01:02 -0400

    added dark theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div>
+            <div>
+                <Image
+                    src="/instagram.svg"
+                    alt=""
+                    height={60}
+                    width={160}
+                    className="rounded-full"
+                />
+            </div>
+        </div>
     )
 }
 export default Header;
