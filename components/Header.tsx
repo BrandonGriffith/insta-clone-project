b8d43c5371800197ed84c4d030b0fcc6330e2500
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-05 18:38:05 -0400

    added heroIcons

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,31 @@ import Image from "next/image";
 const Header = () => {
     return (
         <div>
-            <div>
-                <Image
-                    src="/instagram.svg"
-                    alt=""
-                    height={60}
-                    width={160}
-                    className="rounded-full"
-                />
+            <div className="flex justify-between max-w-7xl">
+                <div className="relative cursor-pointer hidden md:inline-grid">
+                    <Image
+                        src="/instagram.svg"
+                        alt=""
+                        layout="fill"
+                        objectFit="contain"
+                        className="rounded-full"
+                    />
+                </div>
+                <div className="relative cursor-pointer w-10 h-10 md:hidden flex-shrink-0">
+                    <Image
+                        src="/insta-icon.svg"
+                        alt=""
+                        layout="fill"
+                        objectFit="contain"
+                        className="bg-white rounded-lg"
+                    />
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <input type="text" placeholder="Search" />
+                </div>
             </div>
         </div>
     )
