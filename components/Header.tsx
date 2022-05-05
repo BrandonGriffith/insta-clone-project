874fd20633f19ab6386e057c13f3961c9c39b1bc
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-05 18:54:19 -0400

    added tailwindcss forms

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon
+} from "@heroicons/react/outline"
+
 
 const Header = () => {
     return (
         <div>
             <div className="flex justify-between max-w-7xl">
-                <div className="relative cursor-pointer hidden md:inline-grid">
+                <div className="relative w-44 cursor-pointer hidden md:inline-grid">
                     <Image
                         src="/instagram.svg"
                         alt=""
@@ -13,7 +22,7 @@ const Header = () => {
                         className="rounded-full"
                     />
                 </div>
-                <div className="relative cursor-pointer w-10 h-10 md:hidden flex-shrink-0">
+                <div className="relative cursor-pointer w-10 md:hidden flex-shrink-0">
                     <Image
                         src="/insta-icon.svg"
                         alt=""
@@ -22,11 +31,14 @@ const Header = () => {
                         className="bg-white rounded-lg"
                     />
                 </div>
-                <div>
-                    <div>
-
+                <div className="relative mt-1">
+                    <div className="flex absolute inset-y-0 pl-2 items-center">
+                        <SearchIcon className="h-5 w-5 text-white" />
                     </div>
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search"
+                        className="bg-slate-600 text-white 
+                        rounded-lg p-1 block w-full pl-10"
+                    />
                 </div>
             </div>
         </div>
