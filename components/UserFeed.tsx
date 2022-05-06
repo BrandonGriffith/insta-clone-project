425d3efce417cea1d81de064409363a0ea2770d3
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-05-06 19:39:02 -0400

    added faker

--- components/UserFeed.tsx
+++ components/UserFeed.tsx
@@ -1,6 +1,16 @@
+import StoryFeed from "./feed_components/StoryFeed"
+
 const UserFeed = () => {
     return (
-        <div>UserFeed</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
+        xl:grid-cols-3 xl:max-w-6xl mx-auto
+        ">
+            <div className="col-span-2">
+                <StoryFeed />
+            </div>
+            <div>
+            </div>
+        </div>
     )
 }
 export default UserFeed
